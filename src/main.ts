d4214263ea9cc0bf65ce7e60afddbc63aee5d383
sampathIFHE <dsaisampth@ifheindia.org>
2025-11-03 10:56:09 +0530

    sample change

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors({
-    origin: '*',
+    origin: 'http://localhost:5173',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
